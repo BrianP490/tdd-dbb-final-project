75caf5ac3110434f9badb3c6c5a06228fc96e213
BrianP490 <briantech9@gmail.com>
2025-10-19 16:40:33 -0400

    finish exercise 6

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,7 +104,39 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + "-btn"
+    element = context.driver.find_element(By.ID, button_id) 
+    element.click()
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    element_id = 'search_results'
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, element_id),
+            name
+        )
+    )
+    assert(found)
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element_id = "search_results"
+    element = context.driver.find_element(By.ID, element_id) 
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    element_id = "flash_message"
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
 
 ##################################################################
 # This code works because of the following naming convention:
